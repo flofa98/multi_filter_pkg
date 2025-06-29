e59d4ae878f378996518f3695a693927c2fc02b9
Florian Faber <re24m001@technikum-wien.at>
2025-06-29 21:10:27 +0200

    Update multi_filter_localization.launch.py

--- ROS2Colcon/src/multi_filter_pkg/launch/multi_filter_localization.launch.py
+++ ROS2Colcon/src/multi_filter_pkg/launch/multi_filter_localization.launch.py
@@ -1,37 +1,63 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    # Pfad zur Karte
+    tb3_model = 'burger'  
+
+    # Pfade
+    tb3_gazebo_launch = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'launch',
+        'turtlebot3_world.launch.py'
+    )
+
+    nav2_localization = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'launch',
+        'localization_launch.py'
+    )
+
     map_path = os.path.join(
         get_package_share_directory('multi_filter_pkg'),
         'maps',
         'map.yaml'
     )
 
-    # Pfad zur Nav2 localization_launch.py
-    nav2_launch_file = os.path.join(
-        get_package_share_directory('nav2_bringup'),
-        'launch',
-        'localization_launch.py'
-    )
-
     return LaunchDescription([
-        # Starte Karte + AMCL
+        # Gazebo starten mit TurtleBot3
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(nav2_launch_file),
+            PythonLaunchDescriptionSource(tb3_gazebo_launch),
+            launch_arguments={'model': tb3_model}.items()
+        ),
+
+        # Nav2 Localization mit Karte + AMCL
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_localization),
             launch_arguments={'map': map_path}.items()
         ),
 
-        # Starte Node
+        # Filter-Node
         Node(
             package='multi_filter_pkg',
             executable='multi_filter_node',
             name='multi_filter_node',
+            output='screen'
+        ),
+
+        # Initialpose setzen
+        Node(
+            package='geometry_msgs',
+            executable='ros2',
+            name='set_initial_pose',
+            arguments=[
+                'topic', 'pub', '--once', '/initialpose',
+                'geometry_msgs/PoseWithCovarianceStamped',
+                '{header: {frame_id: "map"}, pose: {pose: {position: {x: 0.0, y: 0.0, z: 0.0}, orientation: {z: 0.0, w: 1.0}}, covariance: [0.25, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}}'
+            ],
             output='screen'
         )
     ])
