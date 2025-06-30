ce587776db6f91fa36897052885e71f49fa0f6e7
Florian Faber <re24m001@technikum-wien.at>
2025-06-30 15:58:45 +0200

    Delete ROS2Colcon/build directory

--- ROS2Colcon/build/multi_filter_pkg/CMakeFiles/multi_filter_pkg_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/multi_filter_pkg_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/multi_filter_pkg_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ROS2Colcon/build/multi_filter_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/florianfaber/multifilter/multi_filter_pkg/ROS2Colcon/src/multi_filter_pkg
-# Build directory: /home/florianfaber/multifilter/multi_filter_pkg/ROS2Colcon/build/multi_filter_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
