860ee25f9a7b2e0a933e6472aab3f6e7160cad73
Florian Faber <re24m001@technikum-wien.at>
2025-07-01 00:25:00 +0200

    Update waypoint_nav_node.cpp

--- ROS2Colcon/src/multi_filter_pkg/src/waypoint_nav_node.cpp
+++ ROS2Colcon/src/multi_filter_pkg/src/waypoint_nav_node.cpp
@@ -16,6 +16,8 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2/utils.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 #include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
 #include <random>
