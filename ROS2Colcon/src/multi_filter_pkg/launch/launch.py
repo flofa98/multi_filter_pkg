b05615ee3a572dadac0fd3b628b781381c6d1d1e
Florian Faber <re24m001@technikum-wien.at>
2025-07-05 01:05:48 +0200

    Update launch.py

--- ROS2Colcon/src/multi_filter_pkg/launch/launch.py
+++ ROS2Colcon/src/multi_filter_pkg/launch/launch.py
@@ -1,31 +1,28 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
+    pkg_dir = get_package_share_directory('multi_filter_pkg')
+
     nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('nav2_bringup'),
-                'launch',
-                'tb3_simulation_launch.py'
-            )
+            os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'tb3_simulation_launch.py')
         ),
         launch_arguments={'headless': 'False'}.items()
     )
 
-    prompt_node = Node(
-        package='multi_filter_pkg',
-        executable='initialpose_prompt_node',
-        name='initialpose_prompt_node',
+    prompt = ExecuteProcess(
+        cmd=[os.path.join(pkg_dir, 'scripts', 'initialpose_prompt.sh')],
+        shell=True,
         output='screen'
     )
 
     return LaunchDescription([
         nav2_launch,
-        prompt_node
+        prompt
     ])
+
 
